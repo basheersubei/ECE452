59f396b2caed420dc13b1cf3a79d623767768cee
Basheer Subei <basheersubei@gmail.com>
2015-03-11 04:12:07 -0700

    fixed hw4 q2

--- basheer_452_hw4_q2.m
+++ basheer_452_hw4_q2.m
@@ -10,9 +10,23 @@ omega = w / norm(w)
 omega_hat = [0,-omega(3),omega(2);omega(3),0,-omega(1);-omega(2),omega(1),0]
 
 
-syms t
+t = sym('t');
 theta = 3*t^2 + t
+theta_dot = diff(theta)
+R = expm(simplify(omega_hat * theta))
+p = simplify((eye(3) - R)*q + h* theta * omega)
+twist_exp = [R, p; 0, 0, 0, 1]
+g_ab = simplify(twist_exp * g_0)
+% Part A done
+v = (-1 * cross(omega,q)) + (h * omega)
+twist = [v; omega]
 
-E = expm(omega_hat * theta)
-twist_exp = [E, (eye(3) - E)*q + h* theta * omega; 0, 0, 0, 1]
-g_ab = twist_exp * g_0
+R_T = transpose(R)
+p_hat = [0, -p(3), p(2);p(3), 0, -p(1);-p(2), p(1), 0]
+ad_g_inv = [R_T, -1*R_T * p_hat ; zeros(3,3), R_T]
+V_ab_b = ad_g_inv * twist * theta_dot
+% Part B done
+
+V_ab_s = twist * theta_dot
+
+% Part C done
